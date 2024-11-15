419571228fd3e58b0d5a6d14c325c8fd1ee48be1
airpad <airpad94@gmail.com>
2024-11-15 16:04:34 -0500

    creamos el post pokemon

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const PokemonSchema = new Schema({
+const PokemonSchema = new Schema ({
     id_pokemon:{
         type: Number,
         required:true,
-        unique: true
+        unique:true
     },
     view:{
         type:Boolean,
