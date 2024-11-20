c3b8aa04557c841fdc925e9215b53a46b94c298f
airpad <airpad94@gmail.com>
2024-11-20 15:39:42 -0500

    refactorizando pokemon in team

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -7,4 +7,6 @@ router.post("/",pokemonController.create)
 router.get("/",pokemonController.obtenerPokemon)
 router.get("/:id_pokemon",pokemonController.obtenerPokemonPorIdPokemon)
 router.put("/view/:id_pokemon",pokemonController.avistarPokemonPorId)
+router.put("/catch/:id_pokemon",pokemonController.catchPokemonById)
+router.put("/in_team/:id_pokemon",pokemonController.inTeamPokemonById)
 module.exports = router
